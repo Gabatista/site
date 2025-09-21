183bd10a01d183e391411422b27b4aedee8764c3
Gabatista <gabrielbatista95@hotmail.com>
2025-09-20 22:05:39 -0300

    alteração de redirecionamento

--- port/urls.py
+++ port/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home,name='home'),
     path('lista_projetos/', views.lista_projetos, name='lista_projetos'),
     path('projeto/<str:id_projeto>',views.detalhes_projeto,name='detalhes_projeto'),
-    path('index_english.html',views.home_english,name='english_home'),
+    path('site/port/templates/index.html',views.home_english,name='english_home'),
 ]
